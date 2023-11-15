b7f5bac9d3141613eb43d8156db2b3a87f4c8233
Salah <salah.bouzelzel@gmail.com>
2023-11-15 17:40:10 -0500

    init commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,11 +16,15 @@ urlpatterns = [
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='register', view=views.registration_request, name='register'),
 
     # path for login
+    path(route='login', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
+    # path for index
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,28 +18,37 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
 def about(request):
     return render(request, 'djangoapp/about.html', {})
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
 def contact(request):
     return render(request, 'djangoapp/contact.html', {})
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    return render(request,'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
