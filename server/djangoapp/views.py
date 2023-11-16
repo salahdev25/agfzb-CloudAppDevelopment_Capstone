1cf0e247041c914965089887f7e656fe3deddfd5
Salah <salah.bouzelzel@gmail.com>
2023-11-15 21:11:21 -0500

    git commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, CarMake, CarModel
+#from .restapis import get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def index(request):
+    return render(request, 'djangoapp/index.html', {})
 
 # Create an `about` view to render a static about page
 def about(request):
@@ -53,15 +54,28 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
+
+    json_file = open('./static/json/dealerships.json') # json file load  
+    json_data = json.load(json_file) # json formatted 
+    context['dealerships'] = json_data['dealerships']
+
+    json_file.close()
+
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
-
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    context = {}
+    json_file = open('./static/json/reviews.json') # json file load  
+    json_data = json.load(json_file) # json formatted 
+    context['reviews'] = json_data['reviews']
+    
+    json_file.close()
+    if request.method == "GET":
+        return render(request, 'djangoapp/dealer_details.html', context)
+
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
-
+def add_review(request, dealer_id):
+    pass
